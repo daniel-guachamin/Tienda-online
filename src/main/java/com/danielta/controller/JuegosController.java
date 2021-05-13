ded079a13aaf63536224e71e6e54202dbfe008ed
daniel <danielta874@gmail.com>
2021-05-13 20:20:40 +0200

    añadiendo compra multiple a mi base de datos

--- src/main/java/com/danielta/controller/JuegosController.java
+++ src/main/java/com/danielta/controller/JuegosController.java
@@ -26,10 +26,23 @@ public class JuegosController {
     private int codigo_persona;
     //Array que guardara los juegos que quiera el usuario en el carrito
     private static List<Juego> listaJuegos = new ArrayList();
+    //Array que guardara los juegos que quiera el usuario en la base de datos
+    private static List<JuegosUsuarios> misJuegos = new ArrayList();
+    
     private Juego juego = new Juego();
+    
+    public List<JuegosUsuarios> getMisJuegos() {
+        return misJuegos;
+    }
+
+    public void setMisJuegos(List<JuegosUsuarios> misJuegos) {
+        this.misJuegos = misJuegos;
+    }
+    
+    
 
     public List<Juego> getListaJuegos() {
-        return listaJuegos;
+        return listaJuegos; 
     }
 
     public void setListaJuegos(List<Juego> listaJuegos) {
@@ -59,7 +72,7 @@ public class JuegosController {
     }
 
     public void eliminarJuego(Juego borrar) {
-        
+
         try {
             JuegosController.listaJuegos.remove(borrar);
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Juego eliminado de tu cesta")); //para mostrar mensaje de registro exitoso
@@ -70,11 +83,19 @@ public class JuegosController {
 
     public void agregarCompraJuego1() {
         try {
-            this.juego.setImagen("Detroit.png");
             this.juego.setNombre("Detroit Become Human");
             this.juego.setEstado("Comprar");
+            this.juego.setImagen("Detroit.png");
             this.juego.setPrecio(40);
             JuegosController.listaJuegos.add(juego);
+            
+            this.juegos.setPersona(codigo_persona);
+            this.juegos.setNombre(juego.getNombre());
+            this.juegos.setEstado(juego.getEstado());
+            this.juegos.setImagen(juego.getImagen());
+            this.juegos.setPrecio(juego.getPrecio());
+            JuegosController.misJuegos.add(juegos);
+            
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Juego añadido a tu cesta")); //para mostrar mensaje de registro exitoso
         } catch (Exception e) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Aviso", "Error al añadir a tu cesta!"));
@@ -84,11 +105,19 @@ public class JuegosController {
 
     public void agregarAlquilarJuego1() {
         try {
-            this.juego.setImagen("Detroit.png");
             this.juego.setNombre("Detroit Become Human");
             this.juego.setEstado("Alquilar");
-            this.juego.setPrecio(5);
+            this.juego.setImagen("Detroit.png");
+            this.juego.setPrecio(40);
             JuegosController.listaJuegos.add(juego);
+            
+            this.juegos.setPersona(codigo_persona);
+            this.juegos.setNombre(juego.getNombre());
+            this.juegos.setEstado(juego.getEstado());
+            this.juegos.setImagen(juego.getImagen());
+            this.juegos.setPrecio(juego.getPrecio());
+            JuegosController.misJuegos.add(juegos);
+            
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Juego añadido a tu cesta")); //para mostrar mensaje de registro exitoso
         } catch (Exception e) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Aviso", "Error al añadir a tu cesta!"));
@@ -96,31 +125,17 @@ public class JuegosController {
 
     }
 
-    public void comprarJuego1() {
+    public void finalizarCompra() {
         try {
-            this.juegos.setPersona(codigo_persona);
-            this.juegos.setNombre("Detroit");
-            this.juegos.setEstado("Comprado");
-            this.juegos.setImagen("Detroit.png");
-            this.juegos.setPrecio(40);
-            juegosEJB.create(juegos);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Juego comprado correctamente")); //para mostrar mensaje de registro exitoso
+            for (JuegosUsuarios juegosCarrito : misJuegos) {
+                juegosEJB.create(juegosCarrito);
+            }
+            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Compra realizada correctamente")); //para mostrar mensaje de registro exitoso
         } catch (Exception e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Aviso", "Error al comprar el juego!"));
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Aviso", "Error realizar la compra!"));
         }
-    }
 
-    public void alquilarJuego1() {
-        try {
-            this.juegos.setPersona(codigo_persona);
-            this.juegos.setNombre("Detroit");
-            this.juegos.setEstado("Alquilado disponible 3 días");
-            this.juegos.setImagen("Detroit.png");
-            juegosEJB.create(juegos);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Juego alquilado correctamente")); //para mostrar mensaje de registro exitoso
-        } catch (Exception e) {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Aviso", "Error al alquilar el juego!"));
-        }
     }
 
     public void comprarJuego2() {
